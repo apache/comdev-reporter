8125b5b662d76aadb96a335b84e10ef18ab59065
Daniel Gruno <humbedooh@apache.org>
2019-07-31 14:35:06 +0000

    Only note about mailing list changes if the change involves >= 5 emails.
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1864066 13f79535-47bb-0310-9956-ffa450edef68

--- site/wizard/js/wizard.js
+++ site/wizard/js/wizard.js
@@ -1088,10 +1088,10 @@ function health_tips(data) {
         let a = ml.split('-', 2);
         ml = "%s@%s.apache.org".format(a[1], a[0]);
         let pct_change =Math.floor( 100 * ( (mldata.quarterly[0] - mldata.quarterly[1]) / (mldata.quarterly[1]*1.0) ));
-        if (pct_change > 25) {
+        if (pct_change > 25 && mldata.quarterly[0] > 5) {
             txt += "<span style='color: #080'>- %s had a %u% increase in traffic in the past quarter (%u emails compared to %u)</span><br/>".format(ml, pct_change, mldata.quarterly[0], mldata.quarterly[1]);
         }
-        else if (pct_change < -25) {
+        else if (pct_change < -25  && mldata.quarterly[1] > 5) {
             pct_change = Math.abs(pct_change)
             txt += "<span style='color: #800'>- %s had a %u% decrease in traffic in the past quarter (%u emails compared to %u)</span><br/>".format(ml, pct_change, mldata.quarterly[0], mldata.quarterly[1]);
         }
