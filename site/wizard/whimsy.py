4aefccc990907ac7e236f4e55524fad97d52740a
Daniel Gruno <humbedooh@apache.org>
2019-08-02 16:20:33 +0000

    Add a simple whimsy poster
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1864238 13f79535-47bb-0310-9956-ffa450edef68

---
+++ site/wizard/whimsy.py
@@ -0,0 +1,45 @@
+
+#!/usr/bin/env python
+""" script for publishing a report to whimsy """
+import os
+import sys
+import cgi
+import time
+import json
+import requests
+import re
+
+WHIMSY_URL = 'http://whimsy.local/board/agenda/json/post'
+BASIC_AUTH = os.environ['HTTP_X_WHIMSY_AUTH'] if 'HTTP_X_WHIMSY_AUTH' in os.environ else ""
+USER = os.environ['HTTP_X_AUTHENTICATED_USER'] if 'HTTP_X_AUTHENTICATED_USER' in os.environ else ""
+
+
+def main():
+    
+    # Get project requested and report
+    form = cgi.FieldStorage()
+    project = form.getvalue('project')
+    report = form.getvalue('report')
+    agenda = form.getvalue('agenda')
+    
+    # If auth passed along and project is valid, go look for comments
+    dumps = {'okay': False, 'message': "Could not post to whimsy!"}
+    if BASIC_AUTH and project and report and re.match(r"^board_agenda_\d+_\d+_\d+\.txt$", agenda):
+        js = {
+         'agenda': agenda,
+         'project': project,
+         'report': report,
+         'message': "Publishing %s report via reporter.a.o",
+        }
+        try:
+            rv = requests.post(WHIMSY_URL, headers = {'Authorization': BASIC_AUTH, "Content-Type": "application/json"}, json = js)
+            rv.raise_for_status()
+            dumps = {'okay': True, 'message': "Posted to board agenda!"}
+        except:
+            pass
+    dump = json.dumps(dumps)
+    sys.stdout.write("Content-Type: application/json\r\nContent-Length: %u\r\n\r\n" % (len(dump)))
+    sys.stdout.write(dump)
+
+if __name__ == '__main__':
+    main()
