7cd7bd308461608f849c5aa4433c179928a261ee
Daniel Gruno <humbedooh@apache.org>
2019-08-03 07:19:53 +0000

    forgot to interpolate :(
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1864313 13f79535-47bb-0310-9956-ffa450edef68

--- site/getjson.py
+++ site/getjson.py
@@ -378,7 +378,7 @@ if re.match(r"^[-a-zA-Z0-9_.]+$", user):
     kibble = None
     if oproject:
         try:
-            txt = subprocess.check_output(('%s/site/wizard/kibble.py', oproject))
+            txt = subprocess.check_output(('%s/site/wizard/kibble.py' % RAO_HOME, oproject))
             kibble = json.loads(txt)
         except:
             pass
